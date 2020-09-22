0e88bac7f9234245072cae79a8c5e8cd861a290b
Kumar <mohitkr.mnnit@gmail.com>
2020-09-22 12:13:54 +0530

    removing comment

--- src/main/java/visitcounter/receiver/FootfallDataConsoleReader.java
+++ src/main/java/visitcounter/receiver/FootfallDataConsoleReader.java
@@ -12,27 +12,6 @@ public class FootfallDataConsoleReader {
 			}
 		} catch (Exception e) {
 			throw e;
-		} 	
-	
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/01,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/02,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/02,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/03,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/04,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/05,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/06,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/07,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/07,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/07,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/08,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/14,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/21,08:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/21,09:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/21,11:03:50");
-//		footFallRecordObjectStorer.storeFootFallRecordAsObject("2020/01/21,16:03:50");
-//		
-//		System.out.println("DONEEE1");
-		//System.out.println(FootFallRecordObjectStorer.getFootFallRecords().get(0).getDate());
-		//System.out.println(FootFallRecordObjectStorer.getFootFallRecords().get(0).getTime());		
+		} 		
 	}
 }
